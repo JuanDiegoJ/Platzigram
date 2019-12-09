8e62ca6451ce579ddfe8cbed682d0189f99fe13d
Juan Diego <dippons78@gmail.com>
2019-12-09 17:03:42 -0500

    Clase 29

--- platzigram/posts/views.py
+++ platzigram/posts/views.py
@@ -1,6 +1,8 @@
 #Django
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Utilities
 from posts.form import PostForm
 # Create your views here.
@@ -8,10 +10,12 @@ from posts.form import PostForm
 from posts.models import Post
 
 
-@login_required
-def list_posts(request):
-    posts = Post.objects.all().order_by('-created')
-    return render(request, 'posts/feed.html', {'posts': posts})
+class PostsFeedView(LoginRequiredMixin, ListView):
+    template_name = 'posts/feed.html'
+    model = Post
+    ordering = ('-created')
+    paginate_by = 2
+    context_object_name = 'posts'
 
 @login_required
 def create_posts(request):
@@ -19,7 +23,7 @@ def create_posts(request):
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('feed')
+            return redirect('posts:feed')
     else:
         form = PostForm()
     return render(
--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -3,8 +3,11 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView
+from django.contrib.auth.models import User
+from django.urls import reverse
+from posts.models import Post
 
 from users.forms import ProfileForm, SignupForm
 
@@ -15,7 +18,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
-            return redirect('feed')
+            return redirect('posts:feed')
         else:
             return render(request, 'users/login.html', {'error': 'Invalid username and password'})
     return render(request, 'users/login.html')
@@ -32,7 +35,8 @@ def update_profile(request):
             profile.picture = data['picture']
             profile.save()
             
-            return redirect('update_profile')
+            url = reverse('users:detail', kwargs={'username':request.user.username})
+            return redirect(url)
     else:
         form = ProfileForm()
 
@@ -49,14 +53,14 @@ def update_profile(request):
 @login_required
 def logout_view(request):
     logout(request)
-    return redirect('login')
+    return redirect('users:login')
 
 def signup(request):
     if request.method == 'POST':
        form = SignupForm(request.POST)
        if form.is_valid():
            form.save()
-           return redirect('login')
+           return redirect('users:login')
     else:
         form = SignupForm()
 
@@ -65,3 +69,15 @@ def signup(request):
         template_name='users/signup.html',
         context={'form': form}
     )
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    template_name = 'users/detail.html'
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+    queryset = User.objects.all()
+    context_object_name = 'user'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.get_object()
+        context['posts'] = Post.objects.filter(user=user).order_by('-created')
+        return context
